38bb7d7a30786400fa35b6127d107c86a8606d43
Murtaza <murikhalid.2525@live.com>
2023-01-21 12:59:28 -0500

    Initial Commit

---
+++ khal3470_a1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  khal3470_a1
+//
+//  Created by Murtaza Khalid on 2023-01-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
